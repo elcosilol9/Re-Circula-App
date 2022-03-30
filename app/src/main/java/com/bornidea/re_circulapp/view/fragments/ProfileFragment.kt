5f5c3d0262d93aa8beb26204d08f17afc9b2af4e
Brayan Garrido Martinez <brayangarmar@Brayans-MacBook-Air.local>
2022-03-29 22:46:24 -0600

    Perfil y Logout

--- app/src/main/java/com/bornidea/re_circulapp/view/fragments/ProfileFragment.kt
+++ app/src/main/java/com/bornidea/re_circulapp/view/fragments/ProfileFragment.kt
@@ -1,19 +1,42 @@
 package com.bornidea.re_circulapp.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.bornidea.re_circulapp.R
+import com.bornidea.re_circulapp.databinding.FragmentProfileBinding
+import com.bornidea.re_circulapp.view.activities.Login
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        /**Cerrar sesión*/
+        binding.btLogout.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+            val googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+            googleSignInClient.signOut()
+
+            startActivity(
+                Intent(
+                    requireContext(),
+                    Login::class.java
+                ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            )
+        }
+
+        return binding.root
     }
 }
